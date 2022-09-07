720e77758f5da329544eb23446fd99ac15540165
yeferson castiblanco <yeferson.castiblanco@inoutdelivery.com>
2022-09-07 17:38:51 -0500

    creacion de server en express

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('estoy vivo')
+const express = require("express")
+const app = express()
+const port = 3000
+
+
+app.listen(port, () => {
+  console.log(`your app is ready by http://localhost:${port}`)
+})
