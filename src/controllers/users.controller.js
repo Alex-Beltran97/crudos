af206876c226dec3870e60e200c8ddfce4d4c7f6
yeferson castiblanco <yeferson.castiblanco@inoutdelivery.com>
2022-09-12 23:44:58 -0500

    se creo logica de insercion de usuarios a la DB de pg

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,13 +1,20 @@
 const { models } = require("../databases/sql/pgconfig")
 
 let find = async () => {
-  const results = await models.User.findAll();
+  const results = await models.User.findAll()
   return results
 }
 
-let create = async (body) => {
-  const result = await models.User.create(body)
-  return result
+let create = async (employees) => {
+  try {
+    await models.User.truncate()
+    employees.forEach(async (e) => {
+      await models.User.create(e)
+    })
+    return { message: "se crearon los usuarios correctamente" }
+  } catch (e) {
+    console.error(e.message, "failed insert users")
+  }
 }
 
 module.exports = { find, create }
