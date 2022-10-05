0e15c42e7a22dcf81bb75553da34342ec2d087d8
yeferson castiblanco <yeferson.castiblanco@inoutdelivery.com>
2022-10-04 20:55:47 -0500

    Revert "momento 3"
    This reverts commit 4a670047841a1fc7fa79b425ba61088111e36923.

--- src/controllers/picking.controller.js
+++ src/controllers/picking.controller.js
@@ -1,4 +1,4 @@
-const { ppicking, picking } = require("../models/nosql")
+const { ppicking } = require("../models/nosql")
 const { models } = require("../databases/sql/pgconfig")
 const { handleHttpError } = require("../utils/handleError")
 
@@ -6,25 +6,6 @@ const find = async () => {
   let all = []
   try {
     const data = await ppicking.find({})
-    all = data.map(async (e) => {
-      user = await models.User.findAll({
-        where: { idOperativo: e.idOperativo },
-      })
-      return {
-        ppicking: e,
-        user: user[0],
-      }
-    })
-    let allData = await Promise.all(all).catch(console.error)
-    return allData
-  } catch (e) {
-    handleHttpError(res, "not found picking")
-  }
-}
-const findPiking = async () => {
-  let all = []
-  try {
-    const data = await picking.find({})
     all = data.map(async (e) => {
       user = await models.User.findAll({
         where: { idOperativo: e.idOperativo },
@@ -49,7 +30,9 @@ const listPincking = async () => {
       all.push(e.idPicking)
     })
     let hash = {}
-    all = all.filter((o) => (hash[o] ? false : (hash[o] = true)))
+    all = all.filter((o) =>
+      hash[o] ? false : (hash[o] = true)
+    )
     return all
   } catch (e) {
     console.error(e.message, "error en la lista de ppicking")
@@ -118,45 +101,4 @@ const update = async (idRoll) => {
   }
 }
 
-const createPicking = async (data) => {
-  let result = ""
-  data.forEach(async (e) => {
-    try {
-      user = await models.User.findAll({
-        where: { idOperativo: e.idOperativo },
-      })
-      if (user.length == 0) {
-        console.error("User Not Found")
-      }
-      let body = {
-        idOperativo: user[0].idOperativo,
-        idPicking: e.idPicking,
-        dateAndTime: e.dateAndTime,
-        idLot: e.idLot,
-        idRoll: e.idRoll,
-        rollweight: e.rollweight,
-        client: e.client,
-        referent: e.referent,
-        Weaving: e.Weaving,
-        referralNumber: e.referralNumber,
-        warehouseLocation: e.warehouseLocation,
-      }
-      createCrudos = await picking.create(body)
-      result = { message: "success created" }
-    } catch (e) {
-      result = { message: "error en la creacion", error: e.message }
-      console.error(e.message, "error en la creacion")
-    }
-    return result
-  })
-}
-
-module.exports = {
-  find,
-  create,
-  update,
-  listPincking,
-  orderPicking,
-  createPicking,
-  findPiking,
-}
+module.exports = { find, create, update, listPincking, orderPicking }
--- src/models/nosql/index.js
+++ src/models/nosql/index.js
@@ -1,7 +1,6 @@
 const models = {
   crudos: require("./crudos"),
   ppicking: require("./ppicking"),
-  picking: require("./picking"),
 }
 
 module.exports = models
